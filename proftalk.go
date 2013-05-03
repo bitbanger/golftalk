5c7d25b37ddea226f02145ccb3318bf401adc8e2
Doc <bhqinteractive+github@gmail.com>
2013-05-03 16:51:39 +0200

    eval() function now works with a single case (the conditional)

--- proftalk.go
+++ proftalk.go
@@ -23,8 +23,6 @@ func (e Env) Find(val string) *Env {
 		return &e
 	} else if e.Outer != nil {
 		return e.Outer.Find(val)
-	} else {
-		fmt.Printf("Environment failure!\nCouldn't find '%s'\n", val)
 	}
 
 	return nil
@@ -34,6 +32,16 @@ func NewEnv() *Env {
 	env := &Env{}
 	env.Dict = make(map[string]interface{})
 	return env
+}
+
+func get(lst *list.List, n int) interface{} {
+	obj := lst.Front()
+
+	for i := 0; i < n; i++ {
+		obj = obj.Next()
+	}
+
+	return obj.Value
 }
 
 func tokenize(s string) string {
@@ -135,10 +143,52 @@ func sexpToString(sexp interface{}) string {
 	return ""
 }
 
+func eval(sexp interface{}, env *Env) interface{} {
+	// Is the sexp just a symbol?
+	// If so, let's look it up!
+	symbol, good := sexp.(string)
+	if good {
+		lookupEnv := env.Find(symbol)
+		if lookupEnv != nil {
+			return lookupEnv.Dict[symbol]
+		} else {
+			fmt.Printf("No.\n\t'%s' is an unresolvable symbol.\n", symbol)
+			return nil
+		}
+	}
+
+	// Is the sexp just a list?
+	// If so, let's apply the first symbol as a function to the rest of it!
+	lst, good2 := sexp.(*list.List)
+	if good2 {
+		// The "car" of the list will be a symbol representing a function
+		car, _ := lst.Front().Value.(string)
+
+		switch car {
+			case "insofaras":
+				test := get(lst, 1)
+				conseq := get(lst, 2)
+				alt := get(lst, 3)
+
+				result, wasInt := eval(test, env).(int64)
+
+				if(wasInt && result > 0) {
+					return eval(conseq, env)
+				} else {
+					return eval(alt, env)
+				}
+		}
+	}
+
+	// No other choices left; the sexp must be a literal.
+	// Let's just return it!
+	return sexp
+}
+
 func main() {
-	s := "(1 two (4.12))"
+	s := "(insofaras 0 3.14 4.13)"
 	sexp, _ := parseSexp(splitByRegex(tokenize(s), "\\s+")).(*list.List)
 
-	fmt.Println(sexpToString(sexp))
+	fmt.Println(eval(sexp, NewEnv()))
 }
 
