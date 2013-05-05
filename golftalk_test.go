50848d1d97cb27363a08641c9e89fd978493f58c
Doc <bhqinteractive+github@gmail.com>
2013-05-05 21:13:31 +0200

    Fixed tests to expect ints instead of int64s

--- golftalk_test.go
+++ golftalk_test.go
@@ -4,19 +4,19 @@ import "testing"
 
 var emptyEnv *Env = NewEnv()
 
-func evalExpectInt64(t *testing.T, expr string, expect int64, env *Env) {
+func evalExpectInt(t *testing.T, expr string, expect int, env *Env) {
 	x, err := eval(expr, env)
 	if err != "" {
 		t.Error(expr, "gives error:", err)
 		return
 	}
 	if x == nil {
-		t.Error(expr, "gives nil want an int64")
+		t.Error(expr, "gives nil want an int")
 		return
 	}
-	i, typeOk := x.(int64)
+	i, typeOk := x.(int)
 	if !typeOk {
-		t.Errorf("%s gives %v, want an int64\n", expr, x)
+		t.Errorf("%s gives %v, want an int\n", expr, x)
 		return
 	}
 	if i != expect {
@@ -41,11 +41,11 @@ func TestAddition(t *testing.T) {
 	addEnv := NewEnv()
 	initGlobalEnv(addEnv)
 
-	evalExpectInt64(t,"(+ -5 12)", 7, addEnv)
-	evalExpectInt64(t, "(+ 7 100 99)", 206, addEnv)
-	evalExpectInt64(t, "(+ (+ 1 2) (+ 3 4))", 10, addEnv)
-	evalExpectInt64(t, "(+ 1)", 1, addEnv)
-	evalExpectInt64(t, "(+)", 0, addEnv)
+	evalExpectInt(t,"(+ -5 12)", 7, addEnv)
+	evalExpectInt(t, "(+ 7 100 99)", 206, addEnv)
+	evalExpectInt(t, "(+ (+ 1 2) (+ 3 4))", 10, addEnv)
+	evalExpectInt(t, "(+ 1)", 1, addEnv)
+	evalExpectInt(t, "(+)", 0, addEnv)
 	evalExpectError(t, "(+ hi there)", "Invalid types to add. Must all be int.", addEnv)
 }
 
@@ -53,10 +53,10 @@ func TestSubtraction(t *testing.T) {
 	env := NewEnv()
 	initGlobalEnv(env)
 
-	evalExpectInt64(t,"(- 23 11)", 12, env)
-	evalExpectInt64(t, "(- 55 90 22)", -57, env)
-	evalExpectInt64(t, "(- (- 1 2) (- 3 4))", 0, env)
-	evalExpectInt64(t,"(- 5 )", -5, env)
+	evalExpectInt(t,"(- 23 11)", 12, env)
+	evalExpectInt(t, "(- 55 90 22)", -57, env)
+	evalExpectInt(t, "(- (- 1 2) (- 3 4))", 0, env)
+	evalExpectInt(t,"(- 5 )", -5, env)
 	evalExpectError(t,"(-)", "Need at least 1 int to subtract.", env)
 	evalExpectError(t, "(- go away)", "Invalid types to subtract. Must be int and int.", env)
 }
