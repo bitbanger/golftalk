9c0b6d043861904cd29f42727aa1d3b5dd223b9d
Doc <bhqinteractive+github@gmail.com>
2013-05-07 01:24:30 +0200

    Refactored

--- golftalk.go
+++ golftalk.go
@@ -50,8 +50,8 @@ func MakeEnv(keys []string, vals []interface{}, outer *Env) *Env {
 	return env
 }
 
-// get is a simple utility function to get the nth item from a linked list.
-func get(lst *list.List, n int) interface{} {
+// Get is a simple utility function to Get the nth item from a linked list.
+func Get(lst *list.List, n int) interface{} {
 	obj := lst.Front()
 
 	for i := 0; i < n; i++ {
@@ -61,8 +61,8 @@ func get(lst *list.List, n int) interface{} {
 	return obj.Value
 }
 
-// toSlice converts a linked list into a slice.
-func toSlice(lst *list.List) []interface{} {
+// ToSlice converts a linked list into a slice.
+func ToSlice(lst *list.List) []interface{} {
 	slice := make([]interface{}, lst.Len())
 	i := 0
 	for e := lst.Front(); e != nil; e = e.Next() {
@@ -72,13 +72,8 @@ func toSlice(lst *list.List) []interface{} {
 	return slice
 }
 
-// tokenize returns a string with all parentheses separated by whitespace, and all leading and trailing whitespace removed.
-func tokenize(s string) string {
-	return strings.Trim(strings.Replace(strings.Replace(s, "(", " ( ", -1), ")", " ) ", -1), " ")
-}
-
-// splitByRegex takes a string to split and a regular expression, and returns a linked list of all substrings separated by strings matching the provided regex.
-func splitByRegex(str, regex string) *list.List {
+// SplitByRegex takes a string to split and a regular expression, and returns a linked list of all substrings separated by strings matching the provided regex.
+func SplitByRegex(str, regex string) *list.List {
 	re := regexp.MustCompile(regex)
 	matches := re.FindAllStringIndex(str, -1)
 
@@ -96,10 +91,10 @@ func splitByRegex(str, regex string) *list.List {
 	return result
 }
 
-// atomize infers the data type of a raw string and returns the string converted to this type.
+// Atomize infers the data type of a raw string and returns the string converted to this type.
 // If it fails to safely convert the string, it simply returns it as a string again.
-func atomize(str string) interface{} {
-	// First, try to atomize it as an integer
+func Atomize(str string) interface{} {
+	// First, try to Atomize it as an integer
 	if i, err := strconv.ParseInt(str, 10, 32); err == nil {
 		return int(i)
 	}
@@ -113,9 +108,9 @@ func atomize(str string) interface{} {
 	return str
 }
 
-// isBalanced determines if a string has balanced parentheses.
+// IsBalanced determines if a string has balanced parentheses.
 // This should be used to clean data in the REPL stage before it's parsed.
-func isBalanced(line string) bool {
+func IsBalanced(line string) bool {
 	val := 0
 	for _, c := range line {
 		if c == 40 {
@@ -132,10 +127,10 @@ func isBalanced(line string) bool {
 	return val == 0
 }
 
-// parseSexp takes a linked list of tokens, including parentheses, and uses the parentheses to "un-flatten" the list.
+// ParseSexp takes a linked list of tokens, including parentheses, and uses the parentheses to "un-flatten" the list.
 // The result is a recursively nested set of lists representing the structure of the S-expression.
 // Note: the list is expected to have balanced parentheses.
-func parseSexp(tokens *list.List) interface{} {
+func ParseSexp(tokens *list.List) interface{} {
 	token, _ := tokens.Remove(tokens.Front()).(string)
 
 	if token == "(" {
@@ -145,19 +140,19 @@ func parseSexp(tokens *list.List) interface{} {
 			if firstTok == ")" {
 				break
 			}
-			sexp.PushBack(parseSexp(tokens))
+			sexp.PushBack(ParseSexp(tokens))
 		}
 		tokens.Remove(tokens.Front())
 		return sexp
 	} else {
-		return atomize(token)
+		return Atomize(token)
 	}
 
 	return nil
 }
 
-// sexpToString takes a parsed S-expression and returns a string representation, suitable for printing.
-func sexpToString(sexp interface{}) string {
+// SexpToString takes a parsed S-expression and returns a string representation, suitable for printing.
+func SexpToString(sexp interface{}) string {
 	if i, ok := sexp.(int); ok {
 		return fmt.Sprintf("%d", i)
 	}
@@ -173,7 +168,7 @@ func sexpToString(sexp interface{}) string {
 	if l, ok := sexp.(*list.List); ok {
 		ret := "("
 		for e := l.Front(); e != nil; e = e.Next() {
-			ret = ret + sexpToString(e.Value)
+			ret = ret + SexpToString(e.Value)
 			if e.Next() != nil {
 				ret = ret + " "
 			}
@@ -184,20 +179,21 @@ func sexpToString(sexp interface{}) string {
 	return ""
 }
 
-// eval takes an S-expression and an environment, and returns the most simplified equivalent S-expression.
+// Eval takes an S-expression and an environment, and returns the most simplified equivalent S-expression.
 // Possible ways to simplify an S-expression include returning a literal value if the input was simply that literal value, looking up a symbol in the given environment (and its implied scope chain), and interpreting the S-expression as a function invocation.
-// In the lattermost of evaluation strategies, the function may be provided as a literal or as a symbol referring to a function in the given scope chain; in other words, the first argument has eval recursively applied to it and must yield a function.
-// If an error occurs at any point in the evaluation, eval returns an error string, and the returned value should be disregarded.
-func eval(val interface{}, env *Env) (interface{}, string) {
+// In the lattermost of Evaluation strategies, the function may be provided as a literal or as a symbol referring to a function in the given scope chain; in other words, the first argument has Eval recursively applied to it and must yield a function.
+// If an error occurs at any point in the Evaluation, Eval returns an error string, and the returned value should be disregarded.
+func Eval(val interface{}, env *Env) (interface{}, string) {
 	// Make sure the value is an S-expression
 	sexp := val
 	valStr, wasStr := val.(string)
 	if wasStr {
-		sexp = parseSexp(splitByRegex(tokenize(valStr), "\\s+"))
+		spacedParenStr := strings.Trim(strings.Replace(strings.Replace(valStr, "(", " ( ", -1), ")", " ) ", -1), " ")
+		sexp = ParseSexp(SplitByRegex(spacedParenStr, "\\s+"))
 	}
 	
 	// Is the sexp just a symbol?
-	// If so, let's look it up and evaluate it!
+	// If so, let's look it up and Evaluate it!
 	if symbol, ok := sexp.(string); ok {
 		// Unless it starts with a quote...
 		if strings.HasPrefix(symbol, "'") {
@@ -206,7 +202,7 @@ func eval(val interface{}, env *Env) (interface{}, string) {
 		
 		lookupEnv := env.Find(symbol)
 		if lookupEnv != nil {
-			return eval(lookupEnv.Dict[symbol], env)
+			return Eval(lookupEnv.Dict[symbol], env)
 		} else {
 			return nil, fmt.Sprintf("'%s' not found in scope chain.", symbol)
 		}
@@ -220,31 +216,31 @@ func eval(val interface{}, env *Env) (interface{}, string) {
 
 		switch car {
 			case "insofaras":
-				test := get(lst, 1)
-				conseq := get(lst, 2)
-				alt := get(lst, 3)
+				test := Get(lst, 1)
+				conseq := Get(lst, 2)
+				alt := Get(lst, 3)
 				
 				
-				evalTest, testErr := eval(test, env)
+				EvalTest, testErr := Eval(test, env)
 				
 				if testErr != "" {
 					return nil, testErr
 				}
 				
-				result, wasInt := evalTest.(int)
+				result, wasInt := EvalTest.(int)
 				
 				if !wasInt {
-					return nil, "Test given to conditional evaluated as a non-integer."
+					return nil, "Test given to conditional Evaluated as a non-integer."
 				} else if result > 0 {
-					return eval(conseq, env)
+					return Eval(conseq, env)
 				} else {
-					return eval(alt, env)
+					return Eval(alt, env)
 				}
 			case "you-folks":
 				literal := list.New()
 
 				for e := lst.Front().Next(); e != nil; e = e.Next() {
-					val, valErr := eval(e.Value, env)
+					val, valErr := Eval(e.Value, env)
 					
 					if valErr != "" {
 						return nil, valErr
@@ -255,37 +251,37 @@ func eval(val interface{}, env *Env) (interface{}, string) {
 
 				return literal, ""
 			case "yknow":
-				sym, wasStr := get(lst, 1).(string)
-				symExp := get(lst, 2)
+				sym, wasStr := Get(lst, 1).(string)
+				symExp := Get(lst, 2)
 				
 				if !wasStr {
 					return nil, "Symbol given to define wasn't a string."
 				}
 				
 				// TODO: Is there an elegant way to do this?
-				evalErr := ""
-				env.Dict[sym], evalErr = eval(symExp, env)
+				EvalErr := ""
+				env.Dict[sym], EvalErr = Eval(symExp, env)
 				
-				return nil, evalErr
+				return nil, EvalErr
 			case "apply":
-				evalFunc, _ := eval(get(lst, 1), env)
-				proc, wasFunc := evalFunc.(func(args ...interface{}) (interface{}, string))
-				evalList, _ := eval(get(lst, 2), env)
-				args, wasList := evalList.(*list.List)
+				EvalFunc, _ := Eval(Get(lst, 1), env)
+				proc, wasFunc := EvalFunc.(func(args ...interface{}) (interface{}, string))
+				EvalList, _ := Eval(Get(lst, 2), env)
+				args, wasList := EvalList.(*list.List)
 				
 				if !wasFunc {
-					return nil, "Function given to apply doesn't evaluate as a function."
+					return nil, "Function given to apply doesn't Evaluate as a function."
 				}
 				
 				if !wasList {
-					return nil, "List given to apply doesn't evaluate as a list."
+					return nil, "List given to apply doesn't Evaluate as a list."
 				}
 				
-				argArr := toSlice(args)
+				argArr := ToSlice(args)
 				return proc(argArr...)
 			case "bring-me-back-something-good":
-				vars, wasList := get(lst, 1).(*list.List)
-				exp := get(lst, 2)
+				vars, wasList := Get(lst, 1).(*list.List)
+				exp := Get(lst, 2)
 				
 				if !wasList {
 					return nil, "Symbol list to bind within lambda wasn't a list."
@@ -295,18 +291,18 @@ func eval(val interface{}, env *Env) (interface{}, string) {
 					lambVars := make([]string, vars.Len())
 					for i := range lambVars {
 						// Outer scope handles possible non-string bindables
-						lambVar, _ := get(vars, i).(string)
+						lambVar, _ := Get(vars, i).(string)
 						lambVars[i] = lambVar
 					}
 
 					newEnv := MakeEnv(lambVars, args, env)
 
-					return eval(exp, newEnv)
+					return Eval(exp, newEnv)
 				}, ""
 			case "exit":
 				os.Exit(0)
 			default:
-				evalFunc, funcErr := eval(get(lst, 0), env)
+				EvalFunc, funcErr := Eval(Get(lst, 0), env)
 				
 				if funcErr != "" {
 					return nil, funcErr
@@ -314,12 +310,12 @@ func eval(val interface{}, env *Env) (interface{}, string) {
 				
 				args := make([]interface{}, lst.Len() - 1)
 				for i := range args {
-					// TODO: Do we really need to evaluate here?
-					// Lazy evaluation seems to be the way to go, but then wouldn't we have to evaluate arguments in a more limited scope?
-					args[i], _ = eval(get(lst, i + 1), env)
+					// TODO: Do we really need to Evaluate here?
+					// Lazy Evaluation seems to be the way to go, but then wouldn't we have to Evaluate arguments in a more limited scope?
+					args[i], _ = Eval(Get(lst, i + 1), env)
 				}
 				
-				proc, wasFunc := evalFunc.(func(args ...interface{}) (interface{}, string))
+				proc, wasFunc := EvalFunc.(func(args ...interface{}) (interface{}, string))
 				if wasFunc {
 					return proc(args...)
 				} else {
@@ -334,8 +330,8 @@ func eval(val interface{}, env *Env) (interface{}, string) {
 	return sexp, ""
 }
 
-// initGlobalEnv initializes the hierarchichal "root" environment with a few built-in functions.
-func initGlobalEnv(globalEnv *Env) {
+// InitGlobalEnv initializes the hierarchichal "root" environment with a few built-in functions.
+func InitGlobalEnv(globalEnv *Env) {
 	globalEnv.Dict["+"] = func(args ...interface{}) (interface{}, string) {
 		accumulator := 0
 		for _, val := range args {
@@ -482,7 +478,7 @@ func initGlobalEnv(globalEnv *Env) {
 func main() {
 	globalEnv := NewEnv()
 
-	initGlobalEnv(globalEnv)
+	InitGlobalEnv(globalEnv)
 
 	in := bufio.NewReader(os.Stdin)
 
@@ -499,21 +495,21 @@ func main() {
 			}
 		}
 		
-		if !isBalanced(line) {
+		if !IsBalanced(line) {
 			fmt.Println("No.\n\tUnbalanced parentheses.")
 			continue
 		}
 		
 		if line != "" && line != "\n" {
-			result, evalErr := eval(line, globalEnv)
+			result, EvalErr := Eval(line, globalEnv)
 			
-			if evalErr != "" {
-				fmt.Printf("No.\n\t%s\n", evalErr)
+			if EvalErr != "" {
+				fmt.Printf("No.\n\t%s\n", EvalErr)
 				continue
 			}
 			
 			if result != nil {
-				fmt.Println(sexpToString(result))
+				fmt.Println(SexpToString(result))
 			}
 		}
 	}
