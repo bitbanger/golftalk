a409ed2b84713b61e0eb24edfc9e1ea43d4312f6
Dan Johnson <ComputerDruid@gmail.com>
2013-05-08 01:03:41 +0200

    separate some builtin commands into builtin.go

--- golftalk.go
+++ golftalk.go
@@ -335,214 +335,32 @@ func Eval(val interface{}, env *Env) (interface{}, string) {
 
 // InitGlobalEnv initializes the hierarchichal "root" environment with a few built-in functions.
 func InitGlobalEnv(globalEnv *Env) {
-	globalEnv.Dict["+"] = func(args ...interface{}) (interface{}, string) {
-		accumulator := 0
-		for _, val := range args {
-			i, ok := val.(int)
-			if !ok {
-				return nil, "Invalid types to add. Must all be int."
-			}
-			accumulator += i
-		}
-		return accumulator, ""
-	}
-	
-	globalEnv.Dict["-"] = func(args ...interface{}) (interface{}, string) {
-		switch len(args) {
-			case 0:
-				return nil, "Need at least 1 int to subtract."
-			case 1:
-				val, ok := args[0].(int)
-				if !ok {
-					return nil, "Invalid types to subtract. Must all be int."
-				}
-				return 0 - val, ""
-		}
+	globalEnv.Dict["+"] = add
+	globalEnv.Dict["-"] = subtract
+	globalEnv.Dict["*"] = multiply
+	globalEnv.Dict["/"] = divide
 
-		accumulator := 0
-		for idx, val := range args {
-			i, ok := val.(int)
-			if !ok {
-				return nil, "Invalid types to subtract. Must all be int."
-			}
-			if idx == 0 {
-				accumulator += i
-			} else {
-				accumulator -= i
-			}
-		}
-		return accumulator, ""
-	}
-	
-	globalEnv.Dict["*"] = func(args ...interface{}) (interface{}, string) {
-		a, aok := args[0].(int)
-		b, bok := args[1].(int)
-		
-		if !aok || !bok {
-			return nil, "Invalid types to multiply. Must be int and int."
-		}
+	globalEnv.Dict["or"] = or
+	globalEnv.Dict["and"] = and
+	globalEnv.Dict["not"] = not
 
-		return a * b, ""
-	}
-	
-	globalEnv.Dict["/"] = func(args ...interface{}) (interface{}, string) {
-		a, aok := args[0].(int)
-		b, bok := args[1].(int)
-		
-		if !aok || !bok {
-			return nil, "Invalid types to divide. Must be int and int."
-		}
-		
-		if b == 0 {
-			return nil, "Division by zero is currently unsupported."
-		}
+	globalEnv.Dict["eq?"] = equals
+	globalEnv.Dict["empty?"] = isEmpty
 
-		return a / b, ""
-	}
-	
-	globalEnv.Dict["or"] = func(args ...interface{}) (interface{}, string) {
-		a, aok := args[0].(int)
-		b, bok := args[1].(int)
-		
-		if !aok || !bok {
-			return nil, fmt.Sprintf("Invalid types to compare. Must be int and int. Got %d and %d", a, b)
-		}
-		
-		if a > 0 || b > 0 {
-			return 1, ""
-		}
-		
-		return 0, ""
-	}
-	
-	globalEnv.Dict["and"] = func(args ...interface{}) (interface{}, string) {
-		a, aok := args[0].(int)
-		b, bok := args[1].(int)
-		
-		if !aok || !bok {
-			return nil, "Invalid types to compare. Must be int and int."
-		}
-		
-		if a > 0 && b > 0 {
-			return 1, ""
-		}
-		
-		return 0, ""
-	}
-	
-	globalEnv.Dict["not"] = func(args ...interface{}) (interface{}, string) {
-		a, aok := args[0].(int)
-		
-		if !aok {
-			return nil, "Invalid type to invert. Must be int."
-		}
-		
-		if a > 0 {
-			return 0, ""
-		}
-		
-		return 1, ""
-	}
-	
-	globalEnv.Dict["eq?"] = func(args ...interface{}) (interface{}, string) {
-		if args[0] == args[1] {
-			return 1, ""
-		}
-		
-		return 0, ""
-	}
-	
-	globalEnv.Dict["empty?"] = func(args ...interface{}) (interface{}, string) {
-		lst, ok := args[0].(*list.List)
-		
-		if !ok {
-			return nil, "Invalid type. Can only check if a list is empty."
-		}
-		
-		if lst.Len() == 0 {
-			return 1, ""
-		}
-		
-		return 0, ""
-	}
-	
-	globalEnv.Dict["<"] = func(args ...interface{}) (interface{}, string) {
-		a, aok := args[0].(int)
-		b, bok := args[1].(int)
-		
-		if !aok || !bok {
-			return nil, "Invalid types to compare. Must be int and int."
-		}
+	globalEnv.Dict["car"] = car
+	globalEnv.Dict["come-from-behind"] = comeFromBehind
+	globalEnv.Dict["cons"] = cons
 
-		if a < b {
-			return 1, ""
-		}
-		
-		return 0, ""
-	}
-	
-	globalEnv.Dict["car"] = func(args ...interface{}) (interface{}, string) {
-		lst, ok := args[0].(*list.List)
-		
-		if !ok {
-			return nil, "Invalid type. Can only take the car of a list."
-		}
-		
-		if lst.Len() == 0 {
-			return nil, "Cannot take the car of an empty list."
-		}
-		
-		return lst.Front().Value, ""
-	}
-	
-	globalEnv.Dict["come-from-behind"] = func(args ...interface{}) (interface{}, string) {
-		lst, ok := args[0].(*list.List)
-		
-		if !ok {
-			return nil, "Invalid type. Can only take the cdr of a list."
-		}
-		
-		if lst.Len() == 0 {
-			return nil, "Cannot take the cdr of an empty list."
-		}
-		
-		// TODO: Implement our own S-expressions because Go lists suck
-		newList := list.New()
-		for e := lst.Front().Next(); e != nil; e = e.Next() {
-			newList.PushBack(e.Value)
-		}
-		
-		return newList, ""
-	}
-	
-	globalEnv.Dict["cons"] = func(args ...interface{}) (interface{}, string) {
-		lst, ok := args[1].(*list.List)
-		
-		if !ok {
-			return nil, "Cannot cons to a non-list."
-		}
-		
-		newList := list.New()
-		newList.PushBack(args[0])
-		for e := lst.Front(); e != nil; e = e.Next() {
-			newList.PushBack(e.Value)
-		}
-		
-		return newList, ""
-		
-	}
-	
-	// Neat!
-	globalEnv.Dict[">"] = "(bring-me-back-something-good (a b) (< b a))"
-	globalEnv.Dict["<="] = "(bring-me-back-something-good (a b) (or (< a b) (eq? a b)))"
-	globalEnv.Dict[">="] = "(bring-me-back-something-good (a b) (or (> a b) (eq? a b)))"
+	globalEnv.Dict["<"] = lessThan
+	globalEnv.Dict[">"] = greaterThan
+	globalEnv.Dict["<="] = lessThanOrEqual
+	globalEnv.Dict[">="] = greaterThanOrEqual
 	// Dat spaceship operator
-	globalEnv.Dict["<==>"] = "(bring-me-back-something-good (a b) (insofaras (< a b) -1 (insofaras (> a b) 1 0"
-	
-	globalEnv.Dict["len"] = "(bring-me-back-something-good (lst) (insofaras (empty? lst) 0 (+ 1 (len (come-from-behind lst)))))"
-	
-	globalEnv.Dict["fib"] = "(bring-me-back-something-good (n) (insofaras (< n 2) n (+ (fib (- n 1)) (fib (- n 2)))))"
-	globalEnv.Dict["fact"] = "(bring-me-back-something-good (n) (insofaras (eq? n 0) 1 (* n (fact (- n 1)))))"
+	globalEnv.Dict["<==>"] = spaceship
+
+	globalEnv.Dict["len"] = length
+	globalEnv.Dict["fib"] = fib
+	globalEnv.Dict["fact"] = fact
 }
 
 func main() {
