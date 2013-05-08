c6151a8368df00105cea5995da2eff0141429b23
Dan Johnson <ComputerDruid@gmail.com>
2013-05-08 03:37:42 +0200

    add Len() to Sexps

--- sexp.go
+++ sexp.go
@@ -1,8 +1,26 @@
 package main
 
+import "errors"
+
 type SexpPair struct {
 	val interface{}
 	next interface{}
+}
+
+func (pair *SexpPair) Len() (length int, err error) {
+	if pair == nil {
+		return
+	}
+	if pair.next == nil {
+		//don't care what type next is if it's nil
+		return 1, nil
+	}
+	if next, ok := pair.next.(*SexpPair); ok {
+		length, err = next.Len()
+		length++
+		return
+	}
+	return 1, errors.New("pair does not represent a list")
 }
 
 func toList(items... interface{}) (head *SexpPair) {
