f3c93d8a4e6689df3e9d827751ca00966b5bf3de
Dan Johnson <ComputerDruid@gmail.com>
2013-06-04 13:18:19 -0400

    Eval: split Eval up into several type-specific methods

---
+++ expr.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Expression interface {
+	Eval(env *Env) (result interface{}, nextEnv *Env, err string)
+	String() string
+}
+
+type Symbol string
+//Symbol should implement Expression
+var _ Expression = Symbol("")
+
+func (s Symbol) Eval(env *Env) (result interface{}, nextEnv *Env, err string) {
+	lookupEnv := env.Find(string(s))
+	if lookupEnv != nil {
+		return lookupEnv.Dict[string(s)], env, ""
+	} else {
+		return nil, nil, fmt.Sprintf("'%s' not found in scope chain.", s)
+	}
+}
+
+func (s Symbol) String() string {
+	return string(s)
+}
